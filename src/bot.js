d43fa44beb52c30933a71b75a33027134aac4f56
Khalil Balde <33702527+Khalil-Balde1212@users.noreply.github.com>
2020-12-18 15:11:32 -0500

    Reformated the user command input
    old:
    !AB command command arg arg arg
    
    new:
    !AB command [arg0] [arg1] [arg2]...
    
    Fixed add/remove courses with new formatting

--- src/bot.js
+++ src/bot.js
@@ -81,14 +81,33 @@ client.on('message', async message => {
     if(!message.content.startsWith(prefix)) return;
     
     //command argument setup
-    const args = message.content.slice(prefix.length).trim().split(' ');
+    msg = message.content;
+
+    
+    //find command
+    args = new Array;
+    for(i = 0; i < 3; i++){
+        args[i] = msg.substring(0, msg.indexOf(' ')); //get prefix
+        msg = msg.slice(args[i].length).trim() +  " ";
+    }
+    i = 3;
+    // console.log(msg);
+    while(msg.substring(0, msg.indexOf(']') + 1) != ""){
+        args[i] = msg.substring(0, msg.indexOf(']') + 1);
+        msg = msg.slice(args[i].length).trim() +  " ";
+        args[i] = args[i].replace(/[\[\]']+/g,'')
+        i++;
+    }
+    args.shift();
     const command = args.shift().toLowerCase();
+    // console.log(args);
+    
 
     try{
         await Users.create({ user_id: message.author.id});
         console.log("New User created! ID: " + message.author.id + " " + message.author.tag);
     } catch {
-        console.log("User already exists!");
+        // console.log("User already exists!");
     }
     
 
@@ -136,43 +155,44 @@ client.on('message', async message => {
         case "course":
             switch(args[0]){
                 case "a":
-                case "add":
-                    if(args[1] == null){//check if the user just adds a course
-                        message.channel.send("What is the name of the course you'd like to add?");
-                        const collector = new discord.MessageCollector(message.channel, m => m.author.id === message.author.id, { max: 1, time: 10000 });
-                        // console.log(collector);
-                        collector.on('collect', message => {
-                            // console.log(Courses.findAll({where: {user_id: message.author.id, course_name: message.content}})[0]);
-                            if(Courses.findAll({where: {user_id: message.author.id, course_name: message.content}})[0] == undefined){
-                                Courses.create({user_id: message.author.id, course_name: message.content});
-                                message.channel.send("Added " + message.content + " to your course list!");
+                case "add": //TODO there is probably a smarter way to do this but my brain is unimaginably small
+                    if(args[1] != undefined){ //only adding course
+                        if(args[2] != undefined){ //adding course start and end dates
+                            if(args[3] == undefined) {message.channgel.send("Please input an end date!"); break;}
+                            if(await Courses.findOne({where: {user_id: message.author.id, course_name: args[1]}}) == undefined){
+                                Courses.create({user_id: message.author.id, course_name: args[1], start_date: args[2], end_date: args[3]});
+                                message.channel.send("Added " + args[1] + " to your course list!\nStarts on: " + args[2] + "\nEnds on: " + args[3]);
                             } else{
-                                message.channel.send("You are already enrolled in " + message.content + "!");
+                                message.channel.send("You are already enrolled in " + args[1] + "!");
                             }
-                        });
+                            
+                        } else {
+                            console.log(await Courses.findAll({where: {user_id: message.author.id, course_name: args[1]}}));
+                            if(await Courses.findOne({where: {user_id: message.author.id, course_name: args[1]}}) == undefined){
+                                Courses.create({user_id: message.author.id, course_name: args[1]});
+                                message.channel.send("Added " + args[1] + " to your course list!");
+                            } else{
+                                message.channel.send("You are already enrolled in " + args[1] + "!");
+                            }
+                        }
                     } else {
+                        message.channel.send("No thing dude what the fuck");
                     }
                     break;
 
                 case "r":
                 case "remove":
-                    let filter = m => m.author.id === message.author.id;
-                        message.channel.send("What course would you like to remove?").then(() => {
-                            message.channel.awaitMessages(filter, {
-                                max: 1,
-                                time: 30000,
-                                errors: ['time']
-                            }).then(async message => {
-                                // console.log(Courses.findOne({where: {user_id: message.first().author.id, course_name: message.first().content}}));
-                                if(await Courses.findOne({where: {user_id: message.first().author.id, course_name: message.first().content}}) == undefined){
-                                    message.first().channel.send("You don't take that course!");
-                                } else {
-                                    message.first().channel.send("Removed " + message.first().content + " from your course list");
-                                    Courses.destroy({where: {user_id: message.first().author.id, course_name: message.first().content}});
-                                }
-                            });
-                        });
-
+                    if(args[1] != undefined){
+                        console.log(await Courses.findAll({where: {user_id: message.author.id, course_name: args[1]}}));
+                        if(await Courses.findOne({where: {user_id: message.author.id, course_name: args[1]}}) != undefined){
+                            Courses.destroy({where: {user_id: message.author.id, course_name: args[1]}});
+                            message.channel.send("Removed " + args[1] + " from your course list!");
+                        } else{
+                            message.channel.send("You don't even take " + args[1] + "!");
+                        }
+                    } else {
+                        message.channel.send("No thing dude what the fuck");
+                    }
                     break;
 
                 case "list":
@@ -201,6 +221,7 @@ client.on('message', async message => {
         
             break;
         
+
         default:
             message.channel.send("Command doesnt exist!");
             break;
