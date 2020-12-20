0e71bd1a0bdc4eafcc3acaefe504a18dcf5f1103
Khalil Balde <33702527+Khalil-Balde1212@users.noreply.github.com>
2020-12-20 02:18:57 -0500

    Rudementary assignment commands
    Fixed reaction collector

--- src/Commands/AssignmentCommands.js
+++ src/Commands/AssignmentCommands.js
@@ -34,14 +34,21 @@ module.exports ={
                 const letters = ['🇦', '🇧', '🇨', '🇩', '🇪', '🇫', '🇬', '🇭', '🇮', '🇯'];
                 target = message.mentions.users.first() || message.author;
 
-                const pages = new Array;
-                const page = new Array;
+                var pages = new Array;
+                var page = new Array;
                 
                 if(args[1] == undefined){
                     all_work = (await Assignments.findAll({where: {user_id: target.id}}));
-                    for(i = 0; i < 10; i++){
-                        page[i] = all_work[i];
+
+                    i = 0; j = 0;
+                    while(all_work[j*10 + i]){
+                        for(i = 0; i < 10; i++){
+                            page[i] = all_work[j*10 + i];
+                        }
+                        pages[j] = page;
+                        j++;
                     }
+                    
                 } else {
                     if(await Courses.findOne({where: {user_id: target.id, course_name: args[1]}}) == undefined){
                         message.channel.send('`' + args[1] + "` isn't on your course list!");
@@ -56,11 +63,50 @@ module.exports ={
                 agenda = '';
                 for(i = 0; i < 10; i++){
                     if(page[i] == undefined) break;
-                    agenda += letters[i] + ' ' + page[i].get('course_name') + ': ' + page[i].get('title') + ' - ' + page[i].get('due_date') + '\n\n';
+                    if(page[i].get('complete') == false){
+                        emote = letters[i];
+                    } else {emote = '✅';}
+
+                    agenda += emote + ' ' + page[i].get('course_name') + ': ' + page[i].get('title') + ' - ' + page[i].get('due_date') + '\n\n';
                 }
 
                 messageEmbed.setDescription(agenda);
-                message.channel.send(messageEmbed);
+
+                
+
+                message.channel.send(messageEmbed).then(embed => {
+                    embed.react('◀️');
+                    embed.react('▶️');
+                    for(let i in letters){
+                        if(page[i] == undefined) break;
+                        embed.react(letters[i]);
+                    }
+                    console.log('done reacting');
+                    const collector = embed.createReactionCollector(
+                        (reaction, user) => (['▶️', '◀️'].includes(reaction.emoji.name) || letters.includes(reaction.emoji.name)),
+                        {time: 30000}
+                    )
+
+                    let currentIndex = 0;
+                    collector.on('collect', async (reaction, user) => {
+                        if(user.id != '784662811246723112'){
+                            reaction.users.remove(user.id);
+
+                            for(let i in letters){
+                                if(reaction.emoji.name == letters[i]){
+                                    
+                                }
+                            }
+                        }
+                    })
+
+                    collector.on('end', reaction =>{
+                        embed.reactions.removeAll().catch(error => console.error('Failed to clear reactions: ', error));
+                        embed.react('❌');
+                    })
+                })
+
+
                 break;
 
             default: 
