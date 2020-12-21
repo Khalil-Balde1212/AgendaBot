1164fb7999c1e7df2f384b38f67f0af2818d44c4
Khalil Balde <33702527+Khalil-Balde1212@users.noreply.github.com>
2020-12-21 00:05:49 -0500

    Cleaned up course add
    Cleaned up the course commands code
    
    Added date format checking

--- src/Commands/CourseCommands.js
+++ src/Commands/CourseCommands.js
@@ -9,28 +9,43 @@ module.exports = {
         switch(args[0]){
             case "a":
             case "add": //TODO there is probably a smarter way to do this but my brain is unimaginably small
-                if(args[1] != undefined){ //only adding course
-                    if(args[2] != undefined){ //adding course start and end dates
-                        if(args[3] == undefined) {message.channgel.send("Please input an end date!"); break;}
-                        if(await Courses.findOne({where: {user_id: message.author.id, course_name: args[1]}}) == undefined){
-                            Courses.create({user_id: message.author.id, course_name: args[1], start_date: args[2], end_date: args[3]});
-                            message.channel.send("Added `" + args[1] + "` to your course list!\nStarts on: `" + args[2] + "`\nEnds on: `" + args[3] + "`");
-                        } else{
-                            message.channel.send("You are already enrolled in `" + args[1] + "`!");
-                        }
-                        
-                    } else {
-                        console.log(await Courses.findAll({where: {user_id: message.author.id, course_name: args[1]}}));
-                        if(await Courses.findOne({where: {user_id: message.author.id, course_name: args[1]}}) == undefined){
-                            Courses.create({user_id: message.author.id, course_name: args[1]});
-                            message.channel.send("Added " + args[1] + " to your course list!");
-                        } else{
-                            message.channel.send("You are already enrolled in " + args[1] + "!");
-                        }
-                    }
-                } else {
-                    message.channel.send("No thing dude what the fuck");
+
+                //check if the course is already in the course list
+                if(await Courses.findOne({where: {user_id: message.author.id, course_name: args[1]}}) != undefined){
+                    message.channel.send('`' + args[1] + '` is already on your course list!');
+                    break;
                 }
+
+                //no date inputs
+                if(args[2] == undefined && args[3] == undefined){
+                    Courses.create({user_id: message.author.id, course_name: args[1]});
+                    message.channel.send('Added `' + args[1] + '` to your course list!');
+                    break;
+                }
+
+                //check for improper number of arguments
+                if(args[2] != undefined && args[3] == undefined){
+                    message.channel.send('Proper use of the command is: `!AB course add [course name] [start date] [end date]`')
+                }
+
+                //check if date inputs are in proper date formatting
+                if(checkValidDate(args[2]) == false){
+                    message.channel.send('`' + args[2] + '` is not in mm/dd/yyyy format');
+                    break;
+                }
+
+                if(checkValidDate(args[3]) == false){
+                    message.channel.send('`' + args[3] + '` is not in mm/dd/yyyy format');
+                    break;
+                }
+
+                //succesful!
+                Courses.create({user_id: message.author.id, course_name: args[1], start_date: args[2], start_date: args[3]});
+                message.channel.send(
+                    'added `' + args[1] + '` to your course list! \n' + 
+                    'starts on: `' + args[2] + '`\n' +
+                    'ends on: `' + args[3] + '`'
+                    );
                 break;
 
             case "r":
@@ -72,42 +87,12 @@ module.exports = {
                 break;
 
             case 'setstart':
-                //check if user takes that course
-                if(await Courses.findOne({where: {user_id: message.author.id, course_name: args[1]}}) == undefined){
-                    message.channel.send('`' + args[1] + "` isn't on your course list!");
-                    break;
-                };
-
-                if(args[2].charAt(2) != '/' || args[2].charAt(5) != '/') {
-                    message.channel.send("Please put dates in the form mm/dd/yyyy");
-                } else {
-                    (await Courses.findOne({where: {user_id: message.author.id, course_name: args[1]}})).set('start_date', args[2]).save();
-                }
-
                 break;
                 
             case 'setend':
-                //check if user takes that course
-                if(await Courses.findOne({where: {user_id: message.author.id, course_name: args[1]}}) == undefined){
-                    message.channel.send('`' + args[1] + "` isn't on your course list!");
-                    break;
-                };
-
                 break;
 
             case 'setdates':
-                if(await Courses.findOne({where: {user_id: message.author.id, course_name: args[1]}}) == undefined){
-                    message.channel.send('`' + args[1] + "` isn't on your course list!");
-                    break;
-                };
-
-                if((args[2].charAt(2) != '/' || args[2].charAt(5) != '/') && (args[3].charAt(2) != '/' || args[3].charAt(5) != '/')) {
-                    message.channel.send("Please put dates in the form mm/dd/yyyy");
-                } else {
-                    (await Courses.findOne({where: {user_id: message.author.id, course_name: args[1]}})).set('start_date', args[2]).save();
-                    (await Courses.findOne({where: {user_id: message.author.id, course_name: args[1]}})).set('end_date', args[3]).save();
-                }
-
                 break;
                 
             default:
@@ -116,4 +101,29 @@ module.exports = {
 
         }
     }
+}
+
+function checkValidDate(dateString){
+    // First check for the pattern
+    if(!/^\d{1,2}\/\d{1,2}\/\d{4}$/.test(dateString))
+        return false;
+
+    // Parse the date parts to integers
+    var parts = dateString.split("/");
+    var day = parseInt(parts[1], 10);
+    var month = parseInt(parts[0], 10);
+    var year = parseInt(parts[2], 10);
+
+    // Check the ranges of month and year
+    if(year < 1000 || year > 3000 || month == 0 || month > 12)
+        return false;
+
+    var monthLength = [ 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31 ];
+
+    // Adjust for leap years
+    if(year % 400 == 0 || (year % 100 != 0 && year % 4 == 0))
+        monthLength[1] = 29;
+
+    // Check the range of the day
+    return day > 0 && day <= monthLength[month - 1];
 }
