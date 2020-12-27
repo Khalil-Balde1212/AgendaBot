149141d426d4506f0240cc39a48a777bcba2d131
Khalil Balde <33702527+Khalil-Balde1212@users.noreply.github.com>
2020-12-27 17:58:09 -0500

    Update Test.js
    Put the async in a try catch block

--- src/Commands/Test.js
+++ src/Commands/Test.js
@@ -6,10 +6,15 @@ module.exports = {
         const Users = require('../Models/User')(sequelize, dataTypes);
 
         //create embeded message
-        messageEmbed.setColor((await Users.findOne({where: {user_id: message.author.id}})).get('fav_colour'));
-        messageEmbed.setTitle("test");
-        messageEmbed.setDescription("epic");
+        try{
+            messageEmbed.setColor((await Users.findOne({where: {user_id: message.author.id}})).get('fav_colour'));
+            messageEmbed.setTitle("test");
+            messageEmbed.setDescription("epic");
 
-        message.channel.send(messageEmbed);
+            message.channel.send(messageEmbed);
+
+        } catch{
+            console.log('Error running test command!');
+        }
     }
 }
